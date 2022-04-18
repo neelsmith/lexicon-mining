54f79abeecb917d4cc40d494311f3004367bc6d1
Neel Smith <dnsmith.neel@gmail.com>
2022-04-18 17:06:06 -0400

    Activeate and resolve project

--- scripts/cex.jl
+++ scripts/cex.jl
@@ -1,10 +1,17 @@
-#=
-Read src.cex, and format mainentries.cex
-=#
+# Read src.cex, and format mainentries.cex
+#
 #
 # if running from root of repository:
 f = joinpath(pwd(), "cex", "lewis-short", "src.cex")
 target = joinpath(pwd(),"cex", "lewis-short", "mainentries.cex")
+repo = pwd()
+
+
+using Pkg
+Pkg.activate(repo)
+Pkg.resolve()
+Pkg.instantiate()
+
 lns = readlines(f)
 
 using EzXML
