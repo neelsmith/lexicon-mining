675a0181cd44e9128b753a3cc935dfc8d8d23ae1
Neel Smith <dnsmith.neel@gmail.com>
2022-04-18 18:48:09 -0400

    Count pos-itype combos

--- scripts/pos-itype-pairs.jl
+++ scripts/pos-itype-pairs.jl
@@ -1,6 +1,16 @@
 f = joinpath(pwd(), "cex", "lewis-short", "morphinfo.cex")
-target = joinpath(pwd(), "cex", "lewis-short", "pos-itype-pairs.cex")
+target = joinpath(pwd(), "cex", 
+"lewis-short", "pos-itype-pairs.cex")
+target2 = joinpath(pwd(), "cex", "lewis-short", "pos-itype-pairs-bycount.cex")
+repo = pwd()
 
+
+using Pkg
+Pkg.activate(repo)
+Pkg.resolve()
+Pkg.instantiate()
+
+using SplitApplyCombine
 
 lns = readlines(f)
 
@@ -10,7 +20,21 @@ for ln in lns
     push!(pairlist,cols[4] * "|" * cols[5])
 end
 
+grouped = group(pairlist)
+counts = []
+for k in keys(grouped)
+    push!(counts, (k, length(grouped[k])))
+end
+sorted = sort(counts, by = pr -> pr[1])
 
+tab = map(pr -> string(pr[1], "|", pr[2]), sorted)
 open(target, "w") do io
-    write(io, join(sort(unique(pairlist)),"\n"))
+    write(io,join(tab,"\n"))
+end
+
+
+byfreq = sort(counts, by = pr -> pr[2], rev = true)
+freqtab = map(pr -> string(pr[1], "|", pr[2]), byfreq)
+open(target2, "w") do io
+    write(io,join(freqtab,"\n"))
 end
