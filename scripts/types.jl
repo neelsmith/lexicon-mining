91dc8f0f38623a646f6ec527b1e9c9ecc1e2021b
Neel Smith <dnsmith.neel@gmail.com>
2022-04-18 17:42:04 -0400

    Add list of entry types

--- scripts/types.jl
+++ scripts/types.jl
@@ -1,5 +1,11 @@
 # if running from root of repository:
 f = joinpath(pwd(), "cex", "lewis-short", "mainentries.cex")
+target = joinpath(pwd(), "cex", "lewis-short", "entrytypes.txt")
+
+
+
+########
+
 lns = readlines(f)
 typelist = []
 using EzXML
@@ -15,4 +21,8 @@ for (i,ln) in enumerate(lns)
         println("ERROR ON ENTRY $(i)")
         throw(e)
     end
+end
+
+open(target, "w") do io
+    write(io, join(typelist,"\n"))
 end
