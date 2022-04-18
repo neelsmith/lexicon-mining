8d1577c4d7ac2de6c4b2c56199d5b2f8b4af52e0
Neel Smith <dnsmith.neel@gmail.com>
2022-04-18 15:23:07 -0400

    Add survey script

--- scripts/survey.jl
+++ scripts/survey.jl
@@ -1,18 +1,22 @@
 # if running from root of repository:
 f = joinpath(pwd(), "cex", "lewis-short", "entries.cex")
 lns = readlines(f)
-typelist = []
+morphinfo = ["id|label|lemma|itype"]
 using EzXML
 for (i,ln) in enumerate(lns)
     cols = split(ln, "||")
     try
         doc = parsexml(cols[3])
         t = doc.root["type"]
+        quantlemma = doc.root["key"]
         if t == "main"
 
             # process it...
             # Find element `itype`, get its text contents
-
+            n = findfirst("//itype", doc.root)
+            itype = isnothing(n) ? "" : n.content
+            push!(morphinfo, "$(cols[1])|$(cols[2])|$(quantlemma)|$(itype)")
+    
             # Check for
             # pos, gen, mood tns
         end
@@ -20,4 +24,9 @@ for (i,ln) in enumerate(lns)
         println("ERROR ON ENTRY $(i)")
         throw(e)
     end
+end
+
+
+open("morphinfo.cex", "w") do io
+    write(io, join(morphinfo,"\n"))
 end
