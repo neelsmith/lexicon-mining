4de8e264492e15de0b931afadc8aa24e26f628a3
Documenter.jl <documenter@juliadocs.github.io>
2022-04-20 12:55:43 +0000

    build based on a11b94b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"data/#About-the-data","page":"Data","title":"About the data","text":"","category":"section"},{"location":"#Lexicon-mining","page":"User's guide","title":"Lexicon mining","text":"","category":"section"}]
+}
