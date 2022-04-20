6c61f3b903439415c9da93f5a2b3ec52b4167bc3
Documenter.jl <documenter@juliadocs.github.io>
2022-04-20 13:19:49 +0000

    build based on 719134e

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"data/#About-the-data","page":"Data","title":"About the data","text":"","category":"section"},{"location":"#Lexicon-mining","page":"User's guide","title":"Lexicon mining","text":"","category":"section"}]
+[{"location":"data/#About-the-data","page":"Data","title":"About the data","text":"","category":"section"},{"location":"#Lexicon-mining","page":"User's guide","title":"Lexicon mining","text":"","category":"section"},{"location":"","page":"User's guide","title":"User's guide","text":"Extract morphological data from Perseus XML editions of Lewis-Short and Liddell-Scott-Jones lexica; create delimited-text tables usable with Kanones.jl and Tabulae.jl.","category":"page"}]
 }
