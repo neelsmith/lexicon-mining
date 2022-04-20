7078999bf5ccbaf8470026fe873f3298a3cf94b6
Neel Smith <dnsmith.neel@gmail.com>
2022-04-20 08:45:28 -0400

    Set up project

---
+++ docs/make.jl
@@ -0,0 +1,31 @@
+# Run this from repository root, e.g. with
+# 
+#    julia --project=docs/ docs/make.jl
+#
+# Run this from repository root to serve:
+#
+#   julia -e 'using LiveServer; serve(dir="docs/build")'julia -e 'using LiveServer; serve(dir="docs/build")' 
+#
+using Pkg
+Pkg.activate(".")
+Pkg.instantiate()
+
+using Documenter, DocStringExtensions
+using CitableObject
+using Tabulae
+
+
+makedocs(
+    sitename="LexiconMining.jl",
+    pages = [
+        "Home" => Any[
+            "User's guide" => "index.md",
+            "Data" => "data.md"
+        ]
+        
+    ],
+    )
+
+deploydocs(
+    repo = "github.com/neelsmith/LexiconMining.jl.git",
+)
