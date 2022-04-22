602c04eebe39e62602ec194b23c590ccfe149183
Neel Smith <dnsmith.neel@gmail.com>
2022-04-22 07:47:46 -0400

    Read all LSJ morph files

---
+++ scripts/lsj-readmorph.jl
@@ -0,0 +1,16 @@
+repo = pwd()
+using Pkg
+Pkg.activate(repo)
+Pkg.resolve()
+Pkg.instantiate()
+using LexiconMining
+
+basedir = joinpath(pwd(), "cex", "lsj")
+
+v = Vector{MorphData}()
+
+for i in collect(1:27)
+    f = joinpath(basedir, "morphdata_$(i).cex")
+    append!(v, readlines(f) .|> morphData)
+end
+
