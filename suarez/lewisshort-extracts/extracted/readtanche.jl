762e6524c9028c9a7d6e2d06eb6dbe1cdc5c1f80
Neel Smith <dnsmith.neel@gmail.com>
2024-12-30 08:51:33 -0500

    Tidy up CGPT data

--- suarez/lewisshort-extracts/extracted/readtanche.jl
+++ suarez/lewisshort-extracts/extracted/readtanche.jl
@@ -1,34 +1,41 @@
 dirlist = map(i -> joinpath(pwd(), "suarez", "lewisshort-extracts", "extracted", string("tranche", i)), collect(0:10))
 
-good = 0
-badlist = []
-data = []
-for d in dirlist
-    for f in readdir(d)
-        src = joinpath(d, f)
-        lns = readlines(src)
-        if isempty(lns)
-            @warn("Empty line from file $(f)")
-        elseif length(lns) > 1
-            @info("Multiple lines in $(src)")
-            push!(badlist, src)
-        else
-        
-            cols = split(lns[1], "|") 
-            if length(cols) == 5
-                good = good + 1
-                entry = (seq = cols[1], urn = cols[2], lemma =  cols[3], pos = cols[4], morphology = cols[5])
-                push!(data,entry)
+
+
+function readdata(dirs)
+    good = 0
+    badlist = []
+    data = []
+    for d in dirs
+        for f in readdir(d)
+            src = joinpath(d, f)
+            lns = readlines(src)
+            if isempty(lns)
+                @warn("Empty line from file $(f)")
+            elseif length(lns) > 1
+                @info("Multiple lines in $(src)")
+                push!(badlist, src)
             else
-                @warn("$(length(cols)) columns in $(src)")
             
+                cols = split(lns[1], "|") 
+                if length(cols) == 5
+                    good = good + 1
+                    entry = (seq = cols[1], urn = cols[2], lemma =  cols[3], pos = cols[4], morphology = cols[5])
+                    push!(data,entry)
+                else
+                    @warn("$(length(cols)) columns in $(src)")
+                
+                end
             end
         end
     end
+    (data, badlist)
 end
 
-println("Total good records: $(good)")
-println("Total entries in data set: $(length(data))")
+(data, fails) = readdata(dirlist)
+
+println("Total good records: $(length(data))")
+println("Total failed: $(length(fails))")
 
 using StatsBase
 using OrderedCollections
@@ -43,32 +50,51 @@ sort!(poscounts, byvalue = true, rev = true)
 
 
 using Unicode
-noundata = filter(tpl -> tpl.pos == "noun", data)
+noundata = filter(tpl -> lowercase(strip(tpl.pos)) == "noun", data)
 nouns = map(noundata) do tpl
-    (nsraw,gsraw,genderraw) = split(tpl.morphology,",")
-    ns = Unicode.normalize(strip(nsraw), stripmark = true)
-    gs = Unicode.normalize(strip(gsraw), stripmark = true)
-    gender = Unicode.normalize(strip(genderraw), stripmark = true)
+    cols = split(tpl.morphology,",")
+    if length(cols) > 2
+        nsraw = cols[1]
+        gsraw = cols[2]
+        genderraw = cols[3]
+        ns = Unicode.normalize(strip(nsraw), stripmark = true)
+        gs = Unicode.normalize(strip(gsraw), stripmark = true)
+        gender = Unicode.normalize(strip(genderraw), stripmark = true)
 
-    decl = if endswith(gs, "ae")
-        1
-    elseif endswith(gs, "is")
-        3
-    elseif endswith(gs, "us")
-        4
-    elseif endswith(gs,"ei") && endswith(ns,"es")
-        5
-    elseif endswith(gs, "i")
-        2
+        decl = if endswith(gs, "ae")
+            1
+        elseif endswith(gs, "is")
+            3
+        elseif endswith(gs, "us")
+            4
+        elseif endswith(gs,"ei") && endswith(ns,"es")
+            5
+        elseif endswith(gs, "i")
+            2
+        else
+            0
+        end
+        (urn = tpl.urn, ns = ns, gs = gs, gender = gender, declension = decl)
     else
-        0
+        @warn("$(tpl.morphology): wrong number of columns for noun")
+        nothing
     end
-    (ns = ns, gs = gs, gender = gender, declension = decl)
 end
 
 
 # These results are really good!
 noundecls = map(n -> n.declension, nouns) |> countmap
+
+decl3 = filter(n -> n.declension == 3, nouns)
+
+map(decl3) do n
+    if length(n.gs) < 3
+        @warn("gen.s. too short! $(n.gs) for nom.s. $(n.ns) $(n.urn)")
+        ""
+    else
+        string("-", n.ns[end]," -", n.gs[end-2:end])
+    end
+end
 
 
 
