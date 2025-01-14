88b4d4d31fdee12cb9ea0f85a41d83a58e4a56a3
Neel Smith <dnsmith.neel@gmail.com>
2025-01-14 14:44:08 -0500

    Refine prompt for querying ChatGPT

--- suarez/lewisshort-extracts/extract.jl
+++ suarez/lewisshort-extracts/extract.jl
@@ -9,20 +9,19 @@ Three sections:
 =#
 
 # Uncomment if you're not running in a preconfigured environment:
-#=
+#
 using Pkg
 Pkg.add("HTTP")
 Pkg.add("JSON")
-=#
+#
 
 using Downloads
+
 using HTTP
 using JSON
 
 
-
-
-## 1. Configure suarez
+## 1. Preonfigure suarez or supply when submitting query
 mybearerkey = ""
 
 """Submit a query to suarez and get reply body as a Dict parsed from JSON."""
@@ -95,30 +94,30 @@ end
 
 # 3. Ask chatgpt about Lewis-Short articles
 
-preface = """I have a digital version of Lewis and Short's Latin Dictionary formatted in Markdown. I would like to extract morphological information from articles in the dictionary. Please extract from the article a head word (lemma) and identify the part of speech.  Your reply should be a single line of delimited text.
+preface = """I have a digital version of Lewis and Short's Latin Dictionary formatted in Markdown. I would like to extract morphological information and brief definitions from articles in the dictionary. Please extract from the article a head word (lemma), a short definition (only a few words or phrases please), and identify the part of speech.  Your reply should be a single line of delimited text without any Markdown formatting.
 
 If the article is only a crossreference to another article, please reply with the head word of the article and the second article it points to.
 
-Otherwise, your reply should have three columns separated by a pipe character: head word, part of speech and information specific to each part of speech. If the part of speech is a verb and the verb is a compound of a simplex form, please append the note "(compound)" to the part of speech.
+Otherwise, your reply should have four columns separated by a pipe character: head word, summary definition, part of speech and information specific to each part of speech. If the part of speech is a verb and the verb is a compound of a simplex form, please append the note "(compound)" to the part of speech.
 
-The third column should be a comma-delimited list of information as follows: for nouns, nominative singular, genitive singular, gender; for verbs, conjugation number and principal parts; for adjectives, nominative singular of masculine, feminine and neuter; for prepositions, cases the preposition can be constructed with; for other parts of speech, the label "uninflected".
+The fourth column should be a comma-delimited list of information as follows: for nouns, nominative singular, genitive singular, gender; for verbs, conjugation number and principal parts; for adjectives, nominative singular of masculine, feminine and neuter; for prepositions, cases the preposition can be constructed with; for other parts of speech, the label "uninflected".
 
 """
 
 """Ask ChaptGPT to find morphology data in a Lewis-Short article."""
-function extractmorph(article)
+function extractmorph(article, bearerkey)
 	query = preface * article
-	querysuarez(query) |> messagebody
+	querysuarez(query, bearerkey) |> messagebody
 end
 
 """Submit request to suarez and format reply."""
-function writeanalysisfile(u, ls, outdir)	
+function writeanalysisfile(u, ls, outdir, bearerkey)	
 	article = getarticle(u, ls = ls)
 	if isempty(article) || isnothing(article)
 		"Very bad choice! Too many articles!"
 	else
 		try
-			delimited = string(article.seq, "|", article.urn, "|", extractmorph(article.entry))
+			delimited = string(article.seq, "|", article.urn, "|", extractmorph(article.entry, bearerkey))
 	
 			idval = replace(article.urn, r"[^:]+:" => "")
 			outfile = joinpath(outdir, "$(idval).cex")
@@ -135,22 +134,20 @@ function writeanalysisfile(u, ls, outdir)
 end
 
 
-
-
-
 """Extract morphjological data from a given range of Lewis-Short articles
 and write to delimited file."""
-function extractrange(starti, endi, lexicon, outputroot = "extracted")
+function extractrange(starti, endi, lexicon; outputroot = "extracted", bearerkey = bearerkey)
     for idx in starti:endi
         diridx::Int = floor(Int, idx / 1000)
         outputdir = mkpath(joinpath(outputroot, "tranche$(diridx)"))
-	    writeanalysisfile(lexicon[idx].urn, lexicon, outputdir)
+	    writeanalysisfile(lexicon[idx].urn, lexicon, outputdir, bearerkey)
     end
 end
-
 
 ls  = getls()
 
 
-#extractrange(1999, 2002,ls )
+## Extract data!
+outputdir = joinpath(pwd(), "scratch", "extracts")
+extractrange(11001, 11002, ls; outputroot = outputdir, bearerkey = "")
 
