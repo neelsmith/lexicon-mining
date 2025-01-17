10a76b1be5e1408a6e52745ce929925e3a2da2e1
Neel Smith <dnsmith.neel@gmail.com>
2025-01-17 15:50:54 -0500

    Write nouns to tabulae tables

--- src/tabulae.jl
+++ src/tabulae.jl
@@ -1,3 +1,7 @@
+function summaryfiles()
+    summaryfiles(pwd())
+end
+
 """Find file names in local file system for all files with ChatGPT summaries of Lewis-Short articles.
 $(SIGNATURES)
 """
@@ -7,6 +11,10 @@ function summaryfiles(repo)
         startswith(dir, "tranche")
     end
     map(name -> joinpath(summariesdir, name), tranchenames)
+end
+
+function tabulae(;divider = "|")
+    tabulae(pwd(), divider = divider)
 end
 
 function tabulae(repo; divider = "|")
@@ -21,9 +29,25 @@ function tabulae(repo; divider = "|")
     nouns25 = filter(ln -> occursin("lat25", ln), nounscexlines)
 
     nounheader = join(["StemUrn", "LexicalEntity","Stem","Gender","InflClass"], divider)
+
     commonfile = joinpath(datasetbase, "common", "stems-tables", "nouns", "nouns.cex")
     open(commonfile,"w") do io
         write(io, nounheader * "\n" * join(nounscommon,"\n"))
     end
+    lat23file = joinpath(datasetbase, "lat23", "stems-tables", "nouns", "nouns.cex")
+    open(lat23file,"w") do io
+        write(io, nounheader * "\n" * join(nouns23,"\n"))
+    end
+    lat24file = joinpath(datasetbase, "lat24", "stems-tables", "nouns", "nouns.cex")
+    open(lat24file,"w") do io
+        write(io, nounheader * "\n" * join(nouns24,"\n"))
+    end
+    lat25file = joinpath(datasetbase, "lat25", "stems-tables", "nouns", "nouns.cex")
+    open(lat25file,"w") do io
+        write(io, nounheader * "\n" * join(nouns25,"\n"))
+    end
+
+
+    # Uninflected:
 
 end
