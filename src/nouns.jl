4354d2b2a721f0f0d611938c7fcaf175f9933421
Neel Smith <dnsmith.neel@gmail.com>
2025-01-23 12:56:45 -0500

    Clean up nouns data

--- src/nouns.jl
+++ src/nouns.jl
@@ -64,7 +64,7 @@ end
 noun morphology in type-specific structure.
 $(SIGNATURES)
 """
-function nouns(datatuples; includebad = false)::Union{Vector{LSNoun}, Tuple{Vector{Any}, Vector{Any}}}
+function nouns(datatuples; includebad = false)#::Union{Vector{LSNoun}, Tuple{Vector{Any}, Vector{Any}}}
     noundata = filter(tpl -> tpl.pos == "noun", datatuples)
     good = LSNoun[]
     bad = []
