d48d211aaea091b34d8ea8e9ec0660c19206544c
Neel Smith <dnsmith.neel@gmail.com>
2025-01-30 08:10:19 -0500

    Correct script

--- scripts/rmbadlex.jl
+++ scripts/rmbadlex.jl
@@ -7,26 +7,20 @@ badids = map(t -> replace(t.urn,r".+:" => "") , badlex)
 
 basedir = joinpath(pwd(), "summaries")
 cmds = []
-for id in badids[1:10]
+for id in badids
     idint = parse(Int, replace(id, "n" => ""))
     
     #tranchenum = (idint / 1000) |> round |> Int
     tranchenum = (idint / 1000) |> trunc |> Int
-    #@info(string(idint, " => ", tranchenum) )
+    @info(string(idint, " => ", tranchenum) )
     filename = joinpath(basedir, "tranche$(tranchenum)", "$(id).cex")
     #@info(filename)
     if isfile(filename)
         cmd = "git rm $(filename)"
         push!(cmds, cmd)
+        @info("FOUND file $(filename)")
     else
-        tranchenum = tranchenum -1
-        filename = joinpath(basedir, "tranche$(tranchenum)", "$(id).cex")
-        if isfile(filename)
-            cmd = "git rm $(filename)"
-            push!(cmds, cmd)
-        else
-            @warn("No file: $(filename)")
-        end
+        @warn("No file: $(filename) for id $(id)")
     end
 end
 
