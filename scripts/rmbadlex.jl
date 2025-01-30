d09ceec97ba40f56570864bdc52f1a7961cfa3e6
Neel Smith <dnsmith.neel@gmail.com>
2025-01-30 07:55:29 -0500

    Keep trying to rm bad

--- scripts/rmbadlex.jl
+++ scripts/rmbadlex.jl
@@ -7,18 +7,26 @@ badids = map(t -> replace(t.urn,r".+:" => "") , badlex)
 
 basedir = joinpath(pwd(), "summaries")
 cmds = []
-for id in badids
+for id in badids[1:10]
     idint = parse(Int, replace(id, "n" => ""))
     
-    tranchenum = (idint / 1000) |> round |> Int
-    @info(string(idint, " => ", tranchenum) )
+    #tranchenum = (idint / 1000) |> round |> Int
+    tranchenum = (idint / 1000) |> trunc |> Int
+    #@info(string(idint, " => ", tranchenum) )
     filename = joinpath(basedir, "tranche$(tranchenum)", "$(id).cex")
-    @info(filename)
+    #@info(filename)
     if isfile(filename)
         cmd = "git rm $(filename)"
         push!(cmds, cmd)
     else
-        @warn("No file: $(filename)")
+        tranchenum = tranchenum -1
+        filename = joinpath(basedir, "tranche$(tranchenum)", "$(id).cex")
+        if isfile(filename)
+            cmd = "git rm $(filename)"
+            push!(cmds, cmd)
+        else
+            @warn("No file: $(filename)")
+        end
     end
 end
 
