1663a0489b7514541e4b2ff7244c44de94968bc6
Neel Smith <dnsmith.neel@gmail.com>
2025-01-30 07:46:50 -0500

    Script to rm bogus entries

---
+++ scripts/rmbadlex.jl
@@ -0,0 +1,30 @@
+using LexiconMining
+
+(lex, badlex) = lexicaldata(; includebad = true)
+
+
+badids = map(t -> replace(t.urn,r".+:" => "") , badlex)
+
+basedir = joinpath(pwd(), "summaries")
+cmds = []
+for id in badids
+    idint = parse(Int, replace(id, "n" => ""))
+    
+    tranchenum = (idint / 1000) |> round |> Int
+    @info(string(idint, " => ", tranchenum) )
+    filename = joinpath(basedir, "tranche$(tranchenum)", "$(id).cex")
+    @info(filename)
+    if isfile(filename)
+        cmd = "git rm $(filename)"
+        push!(cmds, cmd)
+    else
+        @warn("No file: $(filename)")
+    end
+end
+
+
+
+
+open("rmbad.sh", "w") do io
+    write(io, join(cmds, "\n"))
+end
